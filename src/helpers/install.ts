dc64bd30551a21b8b9c93d3c07212bdc95ce72d0
therealcoder1337 <78889972+therealcoder1337@users.noreply.github.com>
2025-03-02 19:49:06 +0100

    fix(win): fallback to non .cmd executable when necessary

--- src/helpers/install.ts
+++ src/helpers/install.ts
@@ -1,14 +1,31 @@
 import { spawn } from "child_process";
 import { platform } from "process";
+import { access, constants } from "fs/promises";
 import type { PackageManager } from "../types.js";
 
 const isWin = platform === "win32";
+
+async function hasCmdWin(spawnCmdWin: string) {
+  if (!isWin) {
+    return false;
+  }
+
+  try {
+    await access(spawnCmdWin, constants.F_OK);
+    return true;
+  } catch {
+    return false;
+  }
+}
 
 export default async function install(
   projectPath: string,
   packageManager: PackageManager
 ): Promise<void> {
-  const spawnCmd = `${packageManager}${isWin ? ".cmd" : ""}`;
+  const spawnCmdWin = `${packageManager}.cmd`;
+  const useCmdWin = await hasCmdWin(spawnCmdWin);
+  const spawnCmd = useCmdWin ? spawnCmdWin : packageManager;
+
   const args = ["install"];
 
   console.log(`running ${spawnCmd} ${args.join(" ")}`);
@@ -17,7 +34,7 @@ export default async function install(
     const process = spawn(spawnCmd, args, { cwd: projectPath, stdio: "inherit" });
 
     process.on("close", (code) => {
-      if (code > 0) {
+      if (typeof code !== "number" || code > 0) {
         console.error(`${packageManager} exited with non-zero code`, code);
         reject(new Error("code " + code));
       } else {
